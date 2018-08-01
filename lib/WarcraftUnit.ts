d08a3674e77b1c5632dd86937adfda5242202f91
Unknown <drdgvhbh@gmail.com>
2018-07-31 23:42:07 -0400

    Add Interfaces

---
+++ lib/WarcraftUnit.ts
@@ -0,0 +1,7 @@
+import WarcraftAbility from "./WarcraftAbility";
+
+export default interface WarcraftUnitTile {
+    readonly img: string;
+    readonly title: string;
+    readonly spells: WarcraftAbility[];
+}
